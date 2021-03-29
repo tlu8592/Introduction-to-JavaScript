054aa97f9e116be43ac58ca48fe20a4677b74429
Tony Lu <TonyLu@Tonys-iMac.fios-router.home>
2021-03-28 22:03:40 -0400

    Test completed: annoyingSongFunction

--- index.js
+++ index.js
@@ -239,10 +239,12 @@ Using the annoyingSong function below do the following:
       "{number} bottles of soda on the wall, {number} bottles of soda, take one down pass it around {number left over} bottles of soda on the wall"
 */
 
-function annoyingSong(/*add your code here*/){
-        /*add your code here*/
-  }
+function annoyingSong(num) {
+  return `${num} bottles of soda on the wall, ${num} bottles of soda, take one down pass it around ${num - 1} bottles of soda on the wall`;
+  
+}
 
+console.log(annoyingSong(99));
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 7 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
 
